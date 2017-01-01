0c8c6b490da74f3f0a5f91e38c396951e1856616
Arnold Sobanski <arsobdrakon@gmail.com>
2017-01-01 18:38:29 +0000

    add button to ticket

--- src/Tickets/Application/Query/Result/TicketResult.php
+++ src/Tickets/Application/Query/Result/TicketResult.php
@@ -3,11 +3,15 @@
 namespace Tickets\Application\Query\Result;
 
 use DateTimeInterface;
+use Ramsey\Uuid\UuidInterface;
 use Tickets\Domain\Ticket;
-use Tickets\Domain\TicketsTemplate;
 
 class TicketResult
 {
+    /**
+     * @var UuidInterface
+     */
+    public $uuid;
     /**
      * @var string
      */
@@ -30,17 +34,19 @@ class TicketResult
 
     /**
      * Templates constructor.
+     * @param UuidInterface $uuid
      * @param string $type
      * @param string $status
      * @param DateTimeInterface $createdAt
      * @param DateTimeInterface $updatedAt
      */
-    public function __construct($type, $status, DateTimeInterface $createdAt, DateTimeInterface $updatedAt = null)
+    public function __construct(UuidInterface $uuid, $type, $status, DateTimeInterface $createdAt, DateTimeInterface $updatedAt = null)
     {
         $this->type = $type;
         $this->status = $status;
         $this->createdAt = $createdAt;
         $this->updatedAt = $updatedAt;
+        $this->uuid = $uuid;
     }
 
     /**
@@ -50,6 +56,6 @@ class TicketResult
      */
     public static function createFromTicket(Ticket $ticket)
     {
-        return new self($ticket->getType(), 'not-implemented', $ticket->getCreatedAt(), $ticket->getUpdatedAt());
+        return new self($ticket->getUuid(), $ticket->getType(), 'not-implemented', $ticket->getCreatedAt(), $ticket->getUpdatedAt());
     }
 }
