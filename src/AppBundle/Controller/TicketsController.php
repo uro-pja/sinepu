aa7d62148bd62f1edf82eff2aee6cc0ecef188ba
Arnold Sobanski <arsobdrakon@gmail.com>
2017-01-01 18:16:16 +0000

    exception name in TicketTemplate add

--- src/AppBundle/Controller/TicketsController.php
+++ src/AppBundle/Controller/TicketsController.php
@@ -8,6 +8,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 use Symfony\Component\HttpFoundation\Request;
+use Tickets\Domain\Exception\TicketTemplateAlreadyExistException;
 
 class TicketsController extends Controller
 {
@@ -67,12 +68,12 @@ class TicketsController extends Controller
     {
         $form = $this->createForm(TicketTemplateForm::class);
         $form->handleRequest($request);
-        $errors = null;
+        $errors = [];
         if ($form->isValid()) {
             try {
                 $this->get('sinepu.handler.create_ticket_type')->handle($form->getData());
-            } catch (Exception $e) {
-                $errors= $e->getMessage();
+            } catch (TicketTemplateAlreadyExistException $e) {
+                $errors = [ "name" => "Nazwa Zajeta"];
             }
         }
         $templates = $this->get('sinepu.query.ticket_templates')->findAll();
